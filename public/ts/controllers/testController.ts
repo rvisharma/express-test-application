8c8ed0d97e621b7ddf2f7bc6171d02ff8db0f33a
Rvi <rvisharma91@gmail.com>
2016-01-10 16:06:20 +0530

    reset bootstrap

--- public/ts/controllers/testController.ts
+++ public/ts/controllers/testController.ts
@@ -2,8 +2,7 @@
 module testModule{
     export class TestController {
         constructor(){
-            console.log('testController initiated');
-            alert('test success');
+            console.log('testController initiated success');
         }
     }
     angular.module('testApp').controller('testController', TestController);
